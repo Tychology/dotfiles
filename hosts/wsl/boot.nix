56c49ef5a98c8c20b5d56cd940fc658234a6359c
Tychology <38350366+Tychology@users.noreply.github.com>
2025-08-06 14:15:41 +0200

    fix wsl

---
+++ hosts/wsl/boot.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  boot = {
+    # Kernel
+    kernelPackages = pkgs.linuxPackages_zen;
+    # This is for OBS Virtual Cam Support
+    kernelModules = ["v4l2loopback"];
+    extraModulePackages = [config.boot.kernelPackages.v4l2loopback];
+    # Needed For Some Steam Games
+    kernel.sysctl = {
+      "vm.max_map_count" = 2147483642;
+    };
+    # Bootloader.
+    loader.grub.enable = false;
+    plymouth.enable = true;
+  };
+}
