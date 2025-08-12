bb14432e5a8a03cdc53840c3753dbb08237fed5d
Tychology <38350366+Tychology@users.noreply.github.com>
2025-08-12 21:56:42 +0200

    added overlays because bad download servers

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-#
 {
   inputs = {
     # nixpkgs.url = "github:Tychology/nixpkgs/bash-hotfix";
@@ -10,12 +9,25 @@
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    stylix.url = "github:danth/stylix/release-25.05";
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
-    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    stylix = {
+      url = "github:danth/stylix/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-flatpak = {
+      url = "github:gmodena/nix-flatpak/?ref=latest";
+    };
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
 
     # Required for making sure that Pi-hole continues running if the executing user has no active session.
     linger = {
@@ -61,7 +73,11 @@
             inputs.pihole.nixosModules.${system}.default
 
             ./hosts/${host}/config.nix
-            ({config, ...}: {
+            ({
+              config,
+              pkgs,
+              ...
+            }: {
               nixpkgs.overlays = import ./overlays;
               nixpkgs.config.allowUnfree = true;
               stylix.base16Scheme = builtins.fromJSON ''{"base00":"011d45","base01":"184684","base02":"805aa3","base03":"8fa0b0","base04":"c2b6ba","base05":"ffc5a7","base06":"f3d9c2","base07":"f7cfcd","base08":"b57cb1","base09":"4698d0","base0A":"7995a1","base0B":"8c918d","base0C":"9788c1","base0D":"9f8a9d","base0E":"c57e84","base0F":"dd6b9e"}'';
