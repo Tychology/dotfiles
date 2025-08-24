9eb57daca7e026045a3160a279626232d897abd6
Tycho <tychology@gmail.com>
2025-08-24 18:28:34 +0200

    fix fish variables sourcing

--- config/general/home/fish.nix
+++ config/general/home/fish.nix
@@ -6,6 +6,13 @@
   programs.fish = {
     enable = true;
     generateCompletions = true;
+    shellInit = ''                                                                                                                                                                              │
+      begin                                                                                                                                                                               │
+        set -l oldpath                                                                                                                                                                    │
+        source /etc/fish/setEnvironment.fish                                                                                                                                              │
+        set PATH $oldpath $PATH                                                                                                                                                           │
+      end                                                                                                                                                                                 │
+    '';
     interactiveShellInit = ''
       set fish_greeting ""
       fastfetch
