a8d21c593ba3870d9fee73fbce3a50eba0f26e7a
Tychology <38350366+Tychology@users.noreply.github.com>
2025-12-02 11:02:12 +0100

    fixed xdg portals and added terminal filechooser

--- config/graphical/home/xdg.nix
+++ config/graphical/home/xdg.nix
@@ -1,25 +1,23 @@
 {pkgs, ...}: {
-  environment.pathsToLink = ["/share/xdg-desktop-portal" "/share/applications"];
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
     extraPortals = [
       pkgs.xdg-desktop-portal-gtk
+      pkgs.kdePackages.xdg-desktop-portal-kde
+      pkgs.xdg-desktop-portal-termfilechooser
       pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal
     ];
     configPackages = [
       pkgs.xdg-desktop-portal-gtk
+      pkgs.kdePackages.xdg-desktop-portal-kde
+      pkgs.xdg-desktop-portal-termfilechooser
       pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal
     ];
     config = {
       common = {
-        "org.freedesktop.impl.portal.FileChooser" = ["gtk" "hyprland"];
-        default = ["hyprland" "gtk"];
-      };
-      hyprland = {
-        "org.freedesktop.impl.portal.FileChooser" = ["gtk" "hyprland"];
+        "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
         default = ["hyprland" "gtk"];
       };
     };
