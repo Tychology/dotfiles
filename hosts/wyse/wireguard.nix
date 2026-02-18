c764dfe9711e4a5b6a2d15150e0b3c06fa5dff34
Tycho <tychology@gmail.com>
2026-02-18 19:18:18 +0100

    stop wireguard disconnecting

--- hosts/wyse/wireguard.nix
+++ hosts/wyse/wireguard.nix
@@ -31,8 +31,45 @@
         endpoint = "h98z34vtiojmm.duckdns.org:12408";
         presharedKeyFile = config.age.secrets.wg-f-preshared.path;
         allowedIPs = ["192.168.178.0/24"];
-        persistentKeepalive = 25;
+        persistentKeepalive = 5;
       }
     ];
+  };
+
+  systemd = {
+    services = {
+      "wg-keepalive" = {
+        description = "WireGuard keepalive pinger";
+        after = ["network-online.target"];
+        wants = ["network-online.target"];
+        wantedBy = ["multi-user.target"];
+
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.iputils}/bin/ping -I wg0 -n -i 5 192.168.178.1";
+          Restart = "always";
+          RestartSec = 5;
+          User = "root";
+        };
+      };
+
+      "wg-restart" = {
+        description = "Periodic WireGuard restart";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${pkgs.systemd}/bin/systemctl restart wg-quick-wg0.service";
+        };
+      };
+    };
+
+    timers."wg-restart" = {
+      description = "Timer for periodic WireGuard restart";
+      wantedBy = ["timers.target"];
+      timerConfig = {
+        OnCalendar = "*/6:00:00"; # every 6 hours
+        Unit = "wg-restart.service";
+        Persistent = true;
+      };
+    };
   };
 }
