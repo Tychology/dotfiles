a8afc11d51b07aaf179cb9a48e22d268dacf65b4
Tychology <38350366+Tychology@users.noreply.github.com>
2026-03-19 14:42:49 +0100

    add nameservers to happy

--- hosts/happy/networking.nix
+++ hosts/happy/networking.nix
@@ -7,6 +7,7 @@
   networking.hostName = host;
   networking.timeServers = options.networking.timeServers.default ++ ["pool.ntp.org"];
   networking = {
+    nameservers = ["9.9.9.9" "149.112.112.112"];
     defaultGateway.address = "77.90.40.1";
     firewall.enable = true;
     interfaces = {
